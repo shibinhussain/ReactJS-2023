a0a485d01606247a109621d9045a9cbf92d62e39
Shibin Hussain <shibinhussain82@gmail.com>
2023-01-30 19:20:04 +0530

    final commit

--- react-info-site/src/Components/Navbar.jsx
+++ react-info-site/src/Components/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import logo from '../assets/react-icon.png';
 
 const Navbar = () => {
-  return <h1>NavBar</h1>;
+  return (
+    <nav>
+      <img src={logo} alt="" />
+      <h3>React Infos</h3>
+      <h4>React Project 1</h4>
+    </nav>
+  );
 };
 
 export default Navbar;

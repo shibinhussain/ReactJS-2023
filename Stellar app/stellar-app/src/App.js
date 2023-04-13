572d8c405c7847196e8a728c5c97f5efffd642c3
Shibin <shibin.hussain@iqvia.com>
2023-04-13 16:06:30 +0530

    Upload form added

--- Stellar app/stellar-app/src/App.js
+++ Stellar app/stellar-app/src/App.js
@@ -2,12 +2,22 @@ import "./App.css";
 import { Card } from "./Components/Card";
 import { Navbar } from "./Components/Navbar";
 import { photos } from "./data";
+import UploadForm from "./Components/UploadForm";
+
+import { useState } from "react";
 
 function App() {
+  const [isCollapsed, collapse] = useState(false);
+  const toggle = () => collapse(!isCollapsed);
   return (
     <>
       <Navbar />
       <div className="container text-center mt-5">
+        <button className="btn btn-success float-end" onClick={toggle}>
+          {isCollapsed ? 'Close' : '+Add'}
+        </button>
+        <div className="clearfix mb-4"></div>
+        <UploadForm isVisible={isCollapsed} />
         <h1>Gallery</h1>
         <div className="row">
           {photos.map((photo) => (

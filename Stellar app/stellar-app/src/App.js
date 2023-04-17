40587f59bfb31ece98677d13b29c9660e597a85d
Shibin <shibin.hussain@iqvia.com>
2023-04-17 15:46:35 +0530

    WIP

--- Stellar app/stellar-app/src/App.js
+++ Stellar app/stellar-app/src/App.js
@@ -4,20 +4,53 @@ import { Navbar } from "./Components/Navbar";
 import { photos } from "./data";
 import UploadForm from "./Components/UploadForm";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState();
+  const [input, setInput] = useState({
+    title: null,
+    file: null,
+    path: null,
+  });
   const [isCollapsed, collapse] = useState(false);
   const toggle = () => collapse(!isCollapsed);
+  const [items, setItems] = useState(photos);
+
+  const handleOnChange = (e) => {
+    if (e.target.name === "file") {
+      setInput({
+        ...items,
+        file: e.target.files[0],
+        path: URL.createObjectURL(e.target.files[0]),
+      });
+    } else {
+      setInput({ ...input, title: e.target.value });
+    }
+  };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    setItems([input.path, ...items]);
+  };
+
+  useEffect(() => {
+    setCount(`you have ${items.length} image${items.length > 1 ? "s" : ""}`);
+  }, [items]);
+
   return (
     <>
       <Navbar />
       <div className="container text-center mt-5">
         <button className="btn btn-success float-end" onClick={toggle}>
-          {isCollapsed ? 'Close' : '+Add'}
+          {isCollapsed ? "Close" : "+Add"}
         </button>
         <div className="clearfix mb-4"></div>
-        <UploadForm isVisible={isCollapsed} />
+        <UploadForm
+          isVisible={isCollapsed}
+          onChange={handleOnChange}
+          onSubmit={handleOnSubmit}
+        />
+        {count}
         <h1>Gallery</h1>
         <div className="row">
           {photos.map((photo) => (
--- Stellar app/stellar-app/src/data.js
+++ Stellar app/stellar-app/src/data.js
@@ -4,6 +4,6 @@ export const photos = [
     "https://picsum.photos/id/1003/200/200",
     "https://picsum.photos/id/1004/200/200",
     "https://picsum.photos/id/1005/200/200",
-    "https://picsum.photos/id/1006/200/200",
+    "https://picsum.photos/id/1009/200/200",
 
 ]

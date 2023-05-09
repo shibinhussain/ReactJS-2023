14feaf453c008fe52f85abe5cbc53a34c7811de4
Shibin Hussain <shibinhussain82@gmail.com>
2023-05-09 20:03:50 +0530

    comp updates

--- Backroads app/backroadsapp/src/data.js
+++ Backroads app/backroadsapp/src/data.js
@@ -1,3 +1,7 @@
+import tour1 from "./images/tour-1.jpeg";
+import tour2 from "./images/tour-2.jpeg";
+import tour3 from "./images/tour-3.jpeg";
+
 export const pageLinks = [
   { id: 1, href: "#home", text: "home" },
   { id: 2, href: "#about", text: "about" },
@@ -9,4 +13,58 @@ export const socialLinks = [
   { id: 1, href: "https://www.facebook.com/", icon: "fab fa-facebook" },
   { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
   { id: 3, href: "https://www.squarespace.com/", icon: "fab fa-squarespace" },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: "august 26th, 2023",
+    title: "Tibet Adventure",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "China",
+    duration: 6,
+    amount: "2100",
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: "october 1th, 2023",
+    title: "best of indonesia",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "indonesia",
+    duration: 11,
+    amount: "1400",
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: "september 15th, 2023",
+    title: "explore hong kong",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "hong kong",
+    duration: 8,
+    amount: "5000",
+  },
 ];
